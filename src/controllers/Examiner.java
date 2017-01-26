6b24ef514723d159032de19e9c70d8a17061a355
Hp-Pc <Hp-Pc@HP>
2017-01-26 20:52:31 +0400

    Crazy bug fixed

--- src/controllers/Examiner.java
+++ src/controllers/Examiner.java
@@ -19,7 +19,7 @@ public class Examiner extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
