ec00e18513375ea49ef603e6688e4cc4141a9a06
Hp-Pc <Hp-Pc@HP>
2017-01-29 13:45:46 +0400

    Login page is ready , counting failed attempts is ready

--- src/controllers/loginController.java
+++ src/controllers/loginController.java
@@ -1,26 +1,50 @@
 package controllers;
 
+import db.DataManager;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import pojos.User;
 
 public class loginController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField username;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private PasswordField password;
+
+    @FXML
+    private Button login;
+    @FXML
+    private Button signup;
+    private DataManager dataManager;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
-    
+        dataManager = new DataManager();
+    }
+
+    public void login() {
+        User user = new User();
+        user.setEmail(username.getText());
+        user.setPassword(password.getText());
+        user = dataManager.login(user);
+        if (user == null) {
+            username.setStyle("-fx-border-color : red");
+            password.setStyle("-fx-border-color : red");
+            user = new User();
+            user.setEmail(username.getText());
+            dataManager.countFails(user);
+        } else {
+            System.out.println("**********************************************************");
+        }
+    }
+
+    public void signup() {
+
+    }
 }
--- src/db/DataManager.java
+++ src/db/DataManager.java
@@ -11,6 +11,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pojos.User;
+import types.UserTypes;
 
 /**
  *
@@ -111,35 +115,94 @@ public class DataManager {
                 Answer tempAns = new Answer();
                 tempAns.setAnswerId(resultSet.getInt(1));
                 tempAns.setAnswerData(resultSet.getString(2));
-                tempAns.setAnswerType(AnswerType.TRUE);
+                tempAns.setAnswerType(AnswerType.parseFrom(resultSet.getString(3)));
                 tempAns.setQuestionId(resultSet.getInt(4));
                 answers.add(tempAns);
-
             }
-            
-
         } catch (Exception e) {
             e.printStackTrace(System.err);
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.err);
+            }
         }
 
         return answers;
     }
 
-    public static void main(String[] args) {
+    public User login(User user) {
+        User foundedUser = null;
+        try {
+            connect();
+            String query = "SELECT * FROM users u where u.email = ? and u.password = ? and u.enabled = 1 ";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getPassword());
+            resultSet = preparedStatement.executeQuery();
+            int counter = 0;
+            while (resultSet.next()) {
+                foundedUser = new User();
+                foundedUser.setUserId(resultSet.getInt(1));
+                foundedUser.setName(resultSet.getString(2));
+                foundedUser.setSurname(resultSet.getString(3));
+                foundedUser.setUserType(UserTypes.parseFrom(resultSet.getString(4)));
+                foundedUser.setEmail(resultSet.getString(5));
+                foundedUser.setPassword(resultSet.getString(6));
+                foundedUser.setEnabled((resultSet.getInt(7) == 1));
+                foundedUser.setAttemptCount(resultSet.getByte(8));
+                counter++;
+            }
+            if (counter == 1) {
+                return foundedUser;
+            }
 
-        DataManager dataManager = new DataManager();
-        List<Subject> sj = dataManager.getSubjects();
-        for (Subject s : sj) {
-            System.out.println(s.getSubjectName());
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            try {
+                disconnect();
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
         }
-
-        List<Question> qs = dataManager.getQuestions(sj.get(1));
-        for (Question q : qs) {
-            System.out.println(q);
-        }
+        return null;
     }
 
-    private Answer Answer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void countFails(User user) {
+        try {
+            connect();
+            String query = "SELECT attempts FROM users u where u.email = ? ";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getEmail());
+            resultSet = preparedStatement.executeQuery();
+            int attempts = resultSet.next() ? resultSet.getInt(1) : -1;
+            if(attempts >= 5){
+                query = "update users u set u.attempts = 0  , u.enabled = 0 where u.email = ? ";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, user.getEmail());
+                preparedStatement.execute();
+            }
+            else if (attempts >= 0) {
+                attempts++;
+                query = "update users u set u.attempts = ? where u.email = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1, attempts);
+                preparedStatement.setString(2, user.getEmail());
+                preparedStatement.execute();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            try {
+                disconnect();
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
+        }
+
     }
+
 }
